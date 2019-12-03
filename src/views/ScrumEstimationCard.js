1c52a878be35db119d9bf3dabc59f33d1acd644f
ヴォ スアン ホアン <hoang.vx@outlook.jp>
2019-12-03 13:32:37 +0900

    add settings

--- src/views/ScrumEstimationCard.js
+++ src/views/ScrumEstimationCard.js
@@ -3,17 +3,17 @@ import {
   Container, Row, Col,
   Button,
   ListGroup, ListGroupItem,
-  Modal, ModalBody, ModalHeader,
-  FormSelect
+  Modal, ModalBody, ModalHeader, ModalFooter,
+  FormSelect, FormRadio
 } from 'shards-react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faCog, faInfoCircle } from '@fortawesome/free-solid-svg-icons';
+import { faCog, faInfoCircle, faStar } from '@fortawesome/free-solid-svg-icons';
 
 import Cookies from 'js-cookie';
 import toastr from 'toastr';
 import _ from 'lodash';
 
-const DEFAULT_NUMBER_OF_PLAYPER = 'dnop';
+const DEFAULT_OPTIONS = 'srumpointcaloptions';
 
 const points = [
   0, 1, 2, 3, 5,
@@ -29,6 +29,16 @@ toastr.options = {
   timeOut: 1000
 }
 
+const default_options = {
+  number_of_players: 4,
+  keyboardColorSetting: 'primary',
+  buttonColorSetting: 'primary',
+  pointColorSetting: 'secondary',
+  keyboardColor: 'primary',
+  buttonColor: 'primary',
+  pointColor: 'secondary',
+}
+
 class ScrumEstimationCard extends Component {
   constructor(props) {
     super(props)
@@ -41,6 +51,12 @@ class ScrumEstimationCard extends Component {
       isReady: false,
       openInfo: false,
       openSetting: false,
+      keyboardColorSetting: 'primary',
+      buttonColorSetting: 'primary',
+      pointColorSetting: 'secondary',
+      keyboardColor: 'primary',
+      buttonColor: 'primary',
+      pointColor: 'secondary',
     }
   }
 
@@ -74,8 +90,23 @@ class ScrumEstimationCard extends Component {
   }
 
   setDefault = () => {
-    Cookies.set(DEFAULT_NUMBER_OF_PLAYPER, this.state.number_of_players);
+    const options = {
+      keyboardColor: this.state.keyboardColorSetting,
+      buttonColor: this.state.buttonColorSetting,
+      pointColor: this.state.pointColorSetting,
+      keyboardColorSetting: this.state.keyboardColorSetting,
+      buttonColorSetting: this.state.buttonColorSetting,
+      pointColorSetting: this.state.pointColorSetting,
+      number_of_players: this.state.number_of_players,
+    }
+
+    Cookies.set(DEFAULT_OPTIONS, options);
     toastr.info(`Number of player's defaut is set to ${this.state.number_of_players}`);
+    this.setState({
+      ...options,
+      openSetting: false, 
+    });
+    this.playNewRound();
   }
 
   
@@ -87,10 +118,10 @@ class ScrumEstimationCard extends Component {
   
   componentDidMount = () => {
     document.title = 'Estimation Card | Awesome Tools';
-    const number_of_players = Number(Cookies.get(DEFAULT_NUMBER_OF_PLAYPER)) || 4;
-    this.setState({ 
-      number_of_players: number_of_players,
-      selected_point: Array(Number(number_of_players)).fill(undefined),
+    const options = Cookies.getJSON(DEFAULT_OPTIONS) || default_options;
+    this.setState({
+      ...options,
+      selected_point: Array(Number(options.number_of_players)).fill(undefined),
     })
   }
   render() {
@@ -98,7 +129,8 @@ class ScrumEstimationCard extends Component {
       points, number_of_players, histories, 
       selected_point, valid_points, 
       isReady, isValid,
-      openInfo, openSetting
+      openInfo, openSetting,
+      keyboardColor, buttonColor, pointColor
     } = this.state;
     return (
       <Container>
@@ -110,7 +142,183 @@ class ScrumEstimationCard extends Component {
             <FontAwesomeIcon icon={faCog} className="mx-2" onClick={() => this.setState({ openSetting: true })}/>  
             <Modal className="modal-dialog-centered" open={openSetting} toggle={() => this.setState({ openSetting: !openSetting })}>
               <ModalHeader>Settings</ModalHeader>
-              <ModalBody>Coming Soon!!!</ModalBody>
+              <ModalBody>
+                <Row form>
+                  <Col sm="6">
+                    <label htmlFor="numberOfPlayers">Number of players</label>
+                    <FormSelect
+                      value={number_of_players}
+                      onChange={({ target: { value: number_of_players }}) => this.setState({ number_of_players })}
+                      style={{ fontSize: '16px' }}>
+                      {members_option.map((o, key) => (
+                        <option key={key} value={o}>{o}</option>
+                      ))}
+                    </FormSelect>
+                  </Col>
+                  <Col sm="6">
+                    <p className="mb-2 mt-2">Points color:</p>
+                    <FormRadio
+                      inline
+                      name="pointColor"
+                      checked={this.state.pointColorSetting === "primary"}
+                      onChange={() => {
+                        this.setState({ pointColorSetting: 'primary' });
+                      }}
+                    >
+                      <FontAwesomeIcon className="text-primary" icon={faStar}/>
+                    </FormRadio>
+                    <FormRadio
+                      inline
+                      name="pointColor"
+                      checked={this.state.pointColorSetting === "warning"}
+                      onChange={() => {
+                        this.setState({ pointColorSetting: 'warning' });
+                      }}
+                    >
+                      <FontAwesomeIcon className="text-warning" icon={faStar}/>
+                    </FormRadio>
+                    <FormRadio
+                      inline
+                      name="pointColor"
+                      checked={this.state.pointColorSetting === "success"}
+                      onChange={() => {
+                        this.setState({ pointColorSetting: 'success' });
+                      }}
+                    >
+                      <FontAwesomeIcon className="text-success" icon={faStar}/>
+                    </FormRadio>
+                    <FormRadio
+                      inline
+                      name="pointColor"
+                      checked={this.state.pointColorSetting === "dark"}
+                      onChange={() => {
+                        this.setState({ pointColorSetting: 'dark' });
+                      }}
+                    >
+                      <FontAwesomeIcon className="text-dark" icon={faStar}/>
+                    </FormRadio>
+                    <FormRadio
+                      inline
+                      name="pointColor"
+                      checked={this.state.pointColorSetting === "secondary"}
+                      onChange={() => {
+                        this.setState({ pointColorSetting: 'secondary' });
+                      }}
+                    >
+                      <FontAwesomeIcon className="text-secondary" icon={faStar}/>
+                    </FormRadio>
+                  </Col>
+                  <Col sm="6">
+                    <p className="mb-2 mt-2">Card (keyboard) color:</p>
+                    <FormRadio
+                      inline
+                      name="keyboardColor"
+                      checked={this.state.keyboardColorSetting === "primary"}
+                      onChange={() => {
+                        this.setState({ keyboardColorSetting: 'primary' });
+                      }}
+                    >
+                      <FontAwesomeIcon className="text-primary" icon={faStar}/>
+                    </FormRadio>
+                    <FormRadio
+                      inline
+                      name="keyboardColor"
+                      checked={this.state.keyboardColorSetting === "warning"}
+                      onChange={() => {
+                        this.setState({ keyboardColorSetting: 'warning' });
+                      }}
+                    >
+                      <FontAwesomeIcon className="text-warning" icon={faStar}/>
+                    </FormRadio>
+                    <FormRadio
+                      inline
+                      name="keyboardColor"
+                      checked={this.state.keyboardColorSetting === "success"}
+                      onChange={() => {
+                        this.setState({ keyboardColorSetting: 'success' });
+                      }}
+                    >
+                      <FontAwesomeIcon className="text-success" icon={faStar}/>
+                    </FormRadio>
+                    <FormRadio
+                      inline
+                      name="keyboardColor"
+                      checked={this.state.keyboardColorSetting === "dark"}
+                      onChange={() => {
+                        this.setState({ keyboardColorSetting: 'dark' });
+                      }}
+                    >
+                      <FontAwesomeIcon className="text-dark" icon={faStar}/>
+                    </FormRadio>
+                    <FormRadio
+                      inline
+                      name="keyboardColor"
+                      checked={this.state.keyboardColorSetting === "secondary"}
+                      onChange={() => {
+                        this.setState({ keyboardColorSetting: 'secondary' });
+                      }}
+                    >
+                      <FontAwesomeIcon className="text-secondary" icon={faStar}/>
+                    </FormRadio>
+                  </Col>
+                  <Col sm="6">
+                    <p className="mb-2 mt-2">Button color:</p>
+                    <FormRadio
+                      inline
+                      name="buttonColor"
+                      checked={this.state.buttonColorSetting === "primary"}
+                      onChange={() => {
+                        this.setState({ buttonColorSetting: 'primary' });
+                      }}
+                    >
+                      <FontAwesomeIcon className="text-primary" icon={faStar}/>
+                    </FormRadio>
+                    <FormRadio
+                      inline
+                      name="buttonColor"
+                      checked={this.state.buttonColorSetting === "warning"}
+                      onChange={() => {
+                        this.setState({ buttonColorSetting: 'warning' });
+                      }}
+                    >
+                      <FontAwesomeIcon className="text-warning" icon={faStar}/>
+                    </FormRadio>
+                    <FormRadio
+                      inline
+                      name="buttonColor"
+                      checked={this.state.buttonColorSetting === "success"}
+                      onChange={() => {
+                        this.setState({ buttonColorSetting: 'success' });
+                      }}
+                    >
+                      <FontAwesomeIcon className="text-success" icon={faStar}/>
+                    </FormRadio>
+                    <FormRadio
+                      inline
+                      name="buttonColor"
+                      checked={this.state.buttonColorSetting === "dark"}
+                      onChange={() => {
+                        this.setState({ buttonColorSetting: 'dark' });
+                      }}
+                    >
+                      <FontAwesomeIcon className="text-dark" icon={faStar}/>
+                    </FormRadio>
+                    <FormRadio
+                      inline
+                      name="buttonColor"
+                      checked={this.state.buttonColorSetting === "secondary"}
+                      onChange={() => {
+                        this.setState({ buttonColorSetting: 'secondary' });
+                      }}
+                    >
+                      <FontAwesomeIcon className="text-secondary" icon={faStar}/>
+                    </FormRadio>
+                  </Col>
+                </Row>
+                <ModalFooter className="px-0">
+                  <Button onClick={() => this.setDefault()}>Save</Button>
+                </ModalFooter>
+              </ModalBody>
             </Modal>
             <FontAwesomeIcon icon={faInfoCircle} className="ml-2" onClick={() => this.setState({ openInfo: true })}/>  
             <Modal className="modal-dialog-centered" open={openInfo} toggle={() => this.setState({ openInfo: !openInfo })}>
@@ -135,32 +343,13 @@ class ScrumEstimationCard extends Component {
             </Modal>
           </Col>
         </Row>
-        {/* Welcome Panel for first visited */}
-        <Row className="mb-3">
-          <Col xs="12 p-0">
-            <label className="text-secondary">Change number of players</label>
-          </Col>
-          <Col xs="8" className="px-0">
-            <FormSelect
-              value={number_of_players}
-              onChange={({ target: { value: number_of_players }}) => this.setState({ number_of_players })}
-              style={{ fontSize: '16px' }}>
-              {members_option.map((o, key) => (
-                <option key={key} value={o}>{o}</option>
-              ))}
-            </FormSelect>
-          </Col>
-          <Col xs="4" className="px-0">
-            <Button block outline theme="light" onClick={() => this.setDefault()}>Save</Button>
-          </Col>
-        </Row>
         { number_of_players && (
           <Row className="mb-3">
             {!(isReady && isValid) && selected_point.map((p, idx) => (
               <Col key={idx} className="p-0" style={{ height: '10vh' }}>
                 <Button 
-                  theme={ !isValid && isReady && (p === _.min(valid_points) || p === _.max(valid_points)) ? 'danger': 'secondary' } 
-                  disabled squared block className="h-100 border-light"
+                  theme={ !isValid && isReady && (p === _.min(valid_points) || p === _.max(valid_points)) ? 'danger': pointColor } 
+                  squared block className="h-100 border-light"
                   style={{ fontSize: '2em' }}
                 >{p === undefined ? '-' : p === 999 ? '?' : p}</Button>
               </Col>
@@ -178,7 +367,7 @@ class ScrumEstimationCard extends Component {
           { points.map((p, idx) => (
             <Col key={idx} xs="4" className="px-0 border border-light" style={{ height: '10vh' }}>
               <Button 
-                block squared className="h-100" style={{ fontSize: '2em' }}
+                block squared className="h-100" theme={keyboardColor} style={{ fontSize: '2em' }}
                 onClick={() => this.play(p)}>
                 {p === 999 ? '?' : p}
               </Button>
@@ -187,7 +376,7 @@ class ScrumEstimationCard extends Component {
         </Row>
         <Row className="mb-3">
           <Col className="px-0">
-            <Button block size="lg" onClick={() => this.playNewRound()}>Start Over</Button>
+            <Button block theme={buttonColor} size="lg" onClick={() => this.playNewRound()}>Start Over</Button>
           </Col>
         </Row>
         <Row>
